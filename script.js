dc2f0c07920eb594075907ecac9ff39a7944b750
kenny <kehindeokunola545@gmail.com>
2022-10-28 12:34:19 +0100

    'index.html'

---
+++ script.js
@@ -0,0 +1,43 @@
+var bar = document.querySelector('.bar');
+var time = document.querySelector('.time');
+var links = document.querySelector('.links');
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+bar.onclick = function(){
+    links.style.transform="scale(1)";
+    bar.style.transform = "scale(0)";
+    time.style.transform = "scale(1)";
+    // body.classList.add('overflow');
+    
+    
+}
+time.onclick = function(){
+    links.style.transform="scale(0)";
+    bar.style.transform = "scale(1)";
+    time.style.transform = "scale(0)";
+    // body.style.overflow = ('auto');
+    // body.classList.remove('overflow');
+
+    
+}
+// window.onscroll = function(){
+    
+// if (window.Height <100) {
+//     nav.style.background ="none";
+    
+// } else {
+//     nav.style.background = "black";
+//     nav.style.opacity ="90%";
+// }
+// }
+window.addEventListener('scroll', () => {
+
+
+})
+
+AOS.init({
+    offset:200,
+    duration:500
+
+});  
+
